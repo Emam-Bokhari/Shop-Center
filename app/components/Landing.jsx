529532dc39687a100aaf0169e632b3f02b3883d4
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-16 15:02:30 +0600

    feat(Banner): add Banner component with header image and button

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Navbar from "./Navbar";
+import Banner from "./Banner";
 
 export default function Landing() {
   return (
     <Fragment>
       <Navbar />
+      <Banner />
     </Fragment>
   );
 }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,12 +4,12 @@ import { Fragment } from "react";
 export default function Navbar() {
   return (
     <Fragment>
-      <nav class="flex items-center justify-between w-11/12 lg:w-10/12 max-w-7xl py-5 lg:py-6 mx-auto">
-        <div class="flex gap-5 items-center justify-between lg:w-8/12 text-[#1A1A1A]">
-          <div class="flex items-center gap-2">
+      <nav className="flex items-center justify-between w-11/12 lg:w-10/12 max-w-7xl py-5 lg:py-6 mx-auto">
+        <div className="flex gap-5 items-center justify-between lg:w-8/12 text-[#1A1A1A]">
+          <div className="flex items-center gap-2">
             <Image
               src="/menu.svg"
-              class="lg:hidden w-5 h-5"
+              className="lg:hidden w-5 h-5"
               alt=""
               width={20}
               height={20}
@@ -17,7 +17,7 @@ export default function Navbar() {
             <a href="./index.html">
               <Image
                 src="/lws-logo-black.svg"
-                class="h-10"
+                className="h-10"
                 alt=""
                 width={118}
                 height={40}
@@ -25,17 +25,17 @@ export default function Navbar() {
             </a>
           </div>
         </div>
-        <div class="flex items-center justify-end gap-2 lg:gap-5 lg:w-4/12">
+        <div className="flex items-center justify-end gap-2 lg:gap-5 lg:w-4/12">
           <Image
             src="/avatar.svg"
-            class="hidden lg:block w-[18px] h-[18px]"
+            className="hidden lg:block w-[18px] h-[18px]"
             alt="login acount avatar"
             width={18}
             height={18}
           />
           <Image
             src="shopping-Cart.svg"
-            class="block w-5 h-5"
+            className="block w-5 h-5"
             alt="shopping cart icon"
             width={20}
             height={20}
